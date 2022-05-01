3e47c86af22fe3b6abf2ef715801f6f5c33cd247
Yunhong Min <yunhongmin00@gmail.com>
2022-05-01 20:56:01 +0900

    hack assembly one line command lexer for a command

---
+++ hack_assembly/lexer.py
@@ -0,0 +1,101 @@
+from dataclasses import dataclass
+import re
+from enum import Enum
+
+
+class TokenType(Enum):
+    EOF = -1
+    AT = 1  # @ token
+    INT = 2  # integer
+    VAR = 3  # variable
+
+
+@dataclass
+class Token:
+    type: TokenType
+    text: str
+
+
+class LexerNextTokenException(Exception):
+    pass
+
+
+class Lexer:
+    EOF = -1
+
+    def __init__(self, input_text: str):
+        self._input_text = input_text
+        self._current_char = None
+        self._p = -1
+        self._consume()
+
+    def next_token(self) -> Token:
+        while self._current_char != self.EOF:
+            if self._current_char == '@':
+                self._consume()
+                return Token(TokenType.AT, '@')
+            elif self._is_whitespace():
+                self._whitespace()
+            elif self._is_int():
+                return self._int_token()
+            elif self._is_letter():
+                return self._var_token()
+            else:
+                raise LexerNextTokenException(f'invalid character at {self._p}: {self._current_char}')
+        return Token(TokenType.EOF, '')
+
+    def _consume(self):
+        self._p += 1
+
+        if self._p >= len(self._input_text):
+            self._current_char = self.EOF
+            return
+
+        self._current_char = self._input_text[self._p]
+
+    def _is_whitespace(self) -> bool:
+        if self._current_char == self.EOF:
+            return False
+
+        return re.match(r'^\s$', self._current_char) is not None
+
+    def _whitespace(self):
+        while self._is_whitespace():
+            self._consume()
+
+    def _is_int(self) -> bool:
+        if self._current_char == self.EOF:
+            return False
+
+        # if current char is integer -> True
+        return re.match(r'^\d$', self._current_char) is not None
+
+    def _int_token(self):
+        chars = []
+        while self._is_int():
+            chars.append(self._current_char)
+            self._consume()
+
+            # letter is not proper character for integer token
+            if self._is_letter():
+                raise LexerNextTokenException(f'{self._current_char} char is not proper for integer')
+        return Token(TokenType.INT, ''.join(chars))
+
+    def _is_letter(self) -> bool:
+        if self._current_char == self.EOF:
+            return False
+
+        return re.match(r'^[a-zA-Z]$', self._current_char) is not None
+
+    def _var_token(self):
+        chars = []
+
+        # first letter should be letter
+        chars = [self._current_char]
+        self._consume()
+
+        while self._is_letter() or self._is_int():
+            chars.append(self._current_char)
+            self._consume()
+
+        return Token(TokenType.VAR, ''.join(chars))
---
+++ hack_assembly/test_lexer.py
@@ -0,0 +1,53 @@
+from .lexer import Token, TokenType
+from .lexer import Lexer
+from .lexer import LexerNextTokenException
+
+import pytest
+
+
+class TestLexer:
+    @staticmethod
+    def _lexer_success_test_cases() -> list[tuple]:
+        return [
+            ('@aBc', [
+                Token(TokenType.AT, '@'),
+                Token(TokenType.VAR, 'aBc')
+            ]),
+            ('@132', [
+                Token(TokenType.AT, '@'),
+                Token(TokenType.INT, '132')
+            ]),
+            ('ab123cde', [
+                Token(TokenType.VAR, 'ab123cde'),
+            ]),
+            ('ab123 @ 123', [
+                Token(TokenType.VAR, 'ab123'),
+                Token(TokenType.AT, '@'),
+                Token(TokenType.INT, '123'),
+            ]),
+        ]
+
+    @staticmethod
+    def _lexer_fail_test_cases() -> list[tuple]:
+        return [
+            ('@123abc', LexerNextTokenException),
+        ]
+
+    @pytest.mark.parametrize('input_text,expected', _lexer_success_test_cases())
+    def test_next_token_success(self, input_text, expected):
+        lexer = Lexer(input_text)
+        results = []
+        while (token := lexer.next_token()).type != TokenType.EOF:
+            results.append(token)
+
+        assert results == expected
+
+    @pytest.mark.parametrize('input_text,expected_exception', _lexer_fail_test_cases())
+    def test_next_token_fail(self, input_text, expected_exception):
+        lexer = Lexer(input_text)
+
+        with pytest.raises(expected_exception):
+            while lexer.next_token().type != TokenType.EOF:
+                lexer.next_token()
+
+
